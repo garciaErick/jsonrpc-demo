ca41ae270f77ef8cee53136922cf5d797963adf5
Erick Garcia <erickcubanoide@gmail.com>
2018-04-21 05:17:58 -0600

    Almost done with reference

--- graph/jclient.py
+++ graph/jclient.py
@@ -8,21 +8,28 @@ leaf1 = Node("leaf1",0)
 leaf2 = Node("leaf2",0)
 graph = Node("root", 0,[leaf1, leaf1, leaf2])
 
+
 # Cut-the-corners TCP Client:
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect(('localhost', 50001))
 rpc = JSONRpc(s)
 server = rpc.get_peer_proxy()
 
-# Execute in server:
+Execute in server:
 json_encoded_graph = server.increment(json_encode_graph(graph))
+
+print(json_encode_graph(graph))
+json_graph = json_encode_graph_by_reference(graph)
+print(json_graph)
+graph = json_decode_graph_with_references(json.loads(json_graph))
+graph.show()
+# print("Printing Graph")
 
 print("")
 print("From Client:")
 print("Printing graph received from server")
 graph = json_decode_graph(json.loads(json_encoded_graph))
 graph.show()
-
 rpc.close() # Closes the socket 's' also
 
 
--- graph/node.py
+++ graph/node.py
@@ -1,4 +1,5 @@
 import json
+import uuid
 
 class Node:
     def __init__(self, name, val, children = []):
@@ -23,11 +24,74 @@ def json_decode_graph(json_graph):
         children.append(Node(child["name"], child["val"]))
     return Node(json_graph["name"], json_graph["val"], children)
 
+def json_decode_graph_with_references(json_graph):
+    graph = add_nodes_and_references(json_graph)
+    graph = add_missing_instances_by_reference(graph, graph)
+    graph = remove_keys(graph)
+    return graph
+
+def add_nodes_and_references(json_graph):
+    children = dict()
+    for id, child in json_graph["children"].items():
+        if "children" not in child:
+            children[str(id)] = str(child)
+        else:
+            add_nodes_and_references(child)
+            children[str(id)] = Node(child["name"], child["val"])
+    return Node(json_graph["name"], json_graph["val"], children)
+
+def add_missing_instances_by_reference(graph_with_references, root):
+    #todo: transverse the whole graph
+    if isinstance(graph_with_references.children, dict): 
+        for id, child in graph_with_references.children.items():
+            if isinstance(child, Node): 
+                add_missing_instances_by_reference(child, root)
+            print(id + " => " + str(child))
+            if not isinstance(child, Node):
+                node_reference = find_node_by_id(root, child)
+                graph_with_references.children[id] = node_reference
+    return root
+
+def find_node_by_id(root, ref_id):
+    if isinstance(root, Node):
+        if isinstance(root.children, dict): 
+            for child in root.children.values():
+                find_node_by_id(child, ref_id)
+            if ref_id in root.children.keys():
+                return root.children[ref_id]
+    else:
+        return #not found
+
+def remove_keys(graph_with_ids):
+    if isinstance(graph_with_ids.children, dict): 
+        children = []
+        for child in graph_with_ids.children.values():
+            remove_keys(child)
+            children.append(child)
+        print("printing children")
+        print(children)
+        graph_with_ids.children = children
+        print(graph_with_ids.children)
+        return graph_with_ids
+
 def json_encode_graph(graph):
     children = []
     for child in graph.children:
         children.append(child.__dict__)
         json_encode_graph(child)
+    setattr(graph, 'children', children)
+    json_graph = json.dumps(graph.__dict__)
+    return json_graph
+
+def json_encode_graph_by_reference(graph):
+    children = dict()
+    for child in graph.children:
+        if id(child) in children:
+            guid = uuid.uuid4().hex[:15].upper()
+            id_of_duplicate_child = str(id(child))
+            children[guid] = id_of_duplicate_child
+        children[id(child)] = child.__dict__
+        json_encode_graph_by_reference(child)
     setattr(graph, 'children', children)
     json_graph = json.dumps(graph.__dict__)
     return json_graph
